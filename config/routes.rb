022be8c3e1bb2d4bcbfb0a616d79179914bc3c5a
milaura22@gmail.com <milaura22@gmail.com>
2023-11-15 15:15:21 +0800

    rails yelp mvp

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[index new create]
+  end
+  resources :reviews, only: %i[show edit update destroy]
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  # post "restaurants", to: "restaurants#create"
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # get "restaurants/reviews/new", to: "reviews#new"
+  # post "restaurants/reviews", to: "reviews#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+Restaurant.create(name: "Epicure", address: "bali", phone_number: "46564739", category: "french")
+Restaurant.create(name: "Banana", address: "ubud", phone_number: "74747485", category: "chinese")
+Restaurant.create(name: "Apple", address: "bali", phone_number: "757584", category: "belgian")
+Restaurant.create(name: "Sista Dumpling", address: "kyoto", phone_number: "757584", category: "japanese")
+Restaurant.create(name: "Pholicious", address: "bali", phone_number: "757584",  category: "french")
